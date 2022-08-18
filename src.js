38b5985a547cae566301eb75245354f763f22fbb
R. Starikov <lymon700@gmail.com>
2022-08-18 18:30:07 +0500

    start

---
+++ src.js
@@ -0,0 +1,27 @@
+let preLoadedToDo=[
+    {text: "Проснуться", done: true, color: "green"},
+    {text: "Уснуть", done: false},
+];
+
+function render(todo, elem){
+    elem.innerText = "";
+    
+    todo.forEach(row => {
+        const rowToDo = document.createElement("li");
+        const checkBox = document.createElement("input")
+        checkBox.setAttribute("type", "checkbox");
+        rowToDo.innerText = row.text;
+
+        if(row.done){
+            checkBox.setAttribute("checked", "true");
+        }
+        
+        rowToDo.append(checkBox);
+        elem.append(rowToDo);
+    });
+};
+
+window.onload = () => {
+    const orderedList = document.getElementById("list-To-Do");
+    render(preLoadedToDo, orderedList);
+};
