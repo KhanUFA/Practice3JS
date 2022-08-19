04e61fd479d968bc1de9701c6e560d6048de8c33
R. Starikov <lymon700@gmail.com>
2022-08-19 18:02:38 +0500

    added events on click

--- src.js
+++ src.js
@@ -1,20 +1,23 @@
-let preLoadedToDo=[
-    {text: "Проснуться", done: true, color: "green"},
-    {text: "Уснуть", done: false},
-];
+let tasksDB=[];
+let selectedRow = null;
+const setId = createId();
 
-function render(todo, elem){
-    elem.innerText = "";
+
+function render(todo, targetElement = document.getElementById("list-To-Do")){
+    targetElement.innerText = "";
     
     todo.forEach(row => {
-        addElement(row, elem);
+        addElement(row, targetElement);
     });
 };
 
-window.onload = () => {
-    const orderedList = document.getElementById("list-To-Do");
-    render(preLoadedToDo, orderedList);
-};
+function createId(){
+    let counter = 0;
+
+    return function(){
+        return counter++;
+    }
+}
 
 function addElement(row, element){
     const rowToDo = document.createElement("li");
@@ -23,15 +26,59 @@ function addElement(row, element){
     rowToDo.innerText = row.text;
 
     if(row?.color){
-        rowToDo.setAttribute("data-special", "background-Color: " + row.color);
+        rowToDo.setAttribute("style", "background-color: " + row.color);
     }
- 
+    
     if(row.done){
         checkBox.setAttribute("checked", "");
     } else{
         rowToDo.setAttribute("data-undone", "");
     }
     
+    rowToDo.setAttribute("data-id", row.id);
+
     rowToDo.append(checkBox);
     element.append(rowToDo);
+};
+
+function highlight(target){
+    if(selectedRow){
+        selectedRow.classList.remove("highlight");
+    }
+
+    selectedRow = target;
+    selectedRow.classList.add("highlight");
 }
+
+document.getElementById("list-To-Do").onclick = function(event){
+    if(event.target.tagName == "LI"){
+        highlight(event.target);
+    }
+};
+
+document.getElementById("panelButtons").onclick = function(event){
+    const target = event.target;
+
+    switch(target.value){
+        case "add":
+            let result = prompt("Добавить задачу:");
+            let row = {text: result, done: false, color: "white", id: setId()};
+            tasksDB.push(row);
+            render(tasksDB);
+            break;
+        
+        case "delete":
+            let selectedIndex = selectedRow.getAttribute("data-id");
+            tasksDB.splice(selectedIndex, 1);
+            selectedRow.remove();
+            console.log("Element on pos " + selectedIndex + " delete");
+            break;
+    }
+};
+
+window.onload = () => {
+    const listFromHTML = document.getElementById("list-To-Do");
+    tasksDB.push({text: "Проснуться", done: true, color: "green", id: setId()});
+    tasksDB.push({text: "Улыбнуться", done: false, id: setId()});
+    render(tasksDB, listFromHTML);
+};
